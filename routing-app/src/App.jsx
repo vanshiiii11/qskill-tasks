ac59cd99bd807588c2e9f74dd08db305e47d209d
Vanshika Motwani <vanshi@Vanshikas-MacBook-Pro.local>
2026-07-01 23:02:11 +0530

    fix: lowercase components folder

--- routing-app/src/App.jsx
+++ routing-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './Components/Navbar'
+import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
