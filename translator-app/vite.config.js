c42f8b9cd09bcd162b3917c2a2bb41ea1b28a9a3
Vanshika Motwani <vanshi@Vanshikas-MacBook-Pro.local>
2026-07-01 21:52:00 +0530

    Fix Vercel deployment

--- translator-app/vite.config.js
+++ translator-app/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/qskill-tasks/translator-app/',
   plugins: [react(), tailwindcss()],
 })
